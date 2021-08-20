de1218cbd58b80419d5aa42ddbc9d3125d3d1c8b
jjordan-quantum <constant.space@gmail.com>
2021-08-19 17:33:30 -0700

    fix require statement for compute module

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const request = require("request");
-const compute = require("jobs/compute.js");
+const compute = require("./jobs/compute.js");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
